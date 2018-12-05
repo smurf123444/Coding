8627bb05eb0c883f441d67ebc8627384f4cbab5f
Chris <chris@Chriss-iMac.fios-router.home>
2018-12-04 22:57:19 -0500

    added array.cpp and enum.cpp

--- C++/array.cpp
+++ C++/array.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
- 
+#include <time.h>
+using namespace std;
+void passValue(int value);
+void passArray(int prime[5]);
 int main()
 {
-    // Declare a 10x10 array
+    time_t result = time(NULL);
+    
+        int value = 1;
+    std::cout << "before passValue: " << value << "\n";
+    passValue(value);
+    std::cout << "after passValue: " << value << "\n";
+ 
+    int prime[5] = { 2, 3, 5, 7, 11 };
+    std::cout << "before passArray: " << prime[0] << " " << prime[1] << " " << prime[2] << " " << prime[3] << " " << prime[4] << "\n";
+    passArray(prime);
+    std::cout << "after passArray: " << prime[0] << " " << prime[1] << " " << prime[2] << " " << prime[3] << " " << prime[4] << "\n";
+
+    return 0;
+}
+void passValue(int value) // value is a copy of the argument
+{
+    value = 99; // so changing it here won't change the value of the argument
+}
+ 
+void passArray(int meow[5]) // prime is the actual array
+{
+    time_t result = time(NULL);
+    meow[0] = 1; // so changing it here will change the original argument!
+    meow[1] = 7;
+    meow[2] = 5;
+    meow[3] = 3;
+    meow[4] = 2;
+}
+/*void function() {
+        // Declare a 10x10 array
     const int numRows = 10;
     const int numCols = 10;
     int product[numRows][numCols] = { 0 };
@@ -20,6 +52,5 @@ int main()
  
         std::cout << '\n';
     }
- 
-    return 0;
-}
+ std::cout << "Hey" << std::endl;
+}*/
