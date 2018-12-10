bb6929f7b18c1d3256249c3b904d027c4d59ef73
Christopher Manning <smurf123444@gmail.com>
2018-12-10 00:03:13 -0800

    fixed registration error

--- PHP/registration.php
+++ PHP/registration.php
@@ -6,6 +6,13 @@
 $db = "zero_auth";
 $con = mysqli_connect("127.0.0.1","root","", $db);
 
+function encrypt($user, $pass) {
+  $user = strtoupper($user);
+  $pass = strtoupper($pass);
+  return sha1($user.':'.$pass);
+}
+    
+    
 if(!$con)
 {
     echo "Not connected to DB";
@@ -25,7 +32,7 @@ if (!mysqli_select_db($con,'zero_auth'))
     
     if ($password == $password2)
     {
-        $password = md5($password); //hash the password :D
+        $password = encrypt($username, $_POST['sha_pass_hash']); //hash the password :D
         $sql = "INSERT INTO account (username, sha_pass_hash, expansion, email) VALUES ('$username', '$password', '$expansion', '$email')";
         mysqli_query($con, $sql);
         echo "Query executed";
