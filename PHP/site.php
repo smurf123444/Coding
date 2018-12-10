92c10ee28fec339ad47b0dd194c4b900a65f7258
Christopher Manning <smurf123444@gmail.com>
2018-12-09 19:38:28 -0500

    Update site.php

--- PHP/site.php
+++ PHP/site.php
@@ -68,6 +68,24 @@ catch(PDOException $e) {
 $conn = null;
 echo "</table>";
 ?>
+<?php
+$con=mysqli_connect("localhost","my_user","my_password","my_db");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 
+$sql="SELECT Lastname,Age FROM Persons ORDER BY Lastname";
+$result=mysqli_query($con,$sql);
+
+// Fetch all
+mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+// Free result set
+mysqli_free_result($result);
+
+mysqli_close($con);
+?>
 </body>
 </html>
