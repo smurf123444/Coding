4c5d790f7ff292ef59d5b471e4a386b67b8e8897
Chris <chris@Chriss-iMac.fios-router.home>
2019-01-12 21:57:03 -0500

    mod ch08, ch09 file

--- Python/ch08/ch08#1Initials.py
+++ Python/ch08/ch08#1Initials.py
@@ -1,12 +1,19 @@
 # define main function
 
 def main():
+    
     # prompt user for input of name to be initiald
     name = input('Type your name and press ENTER. ')
+
+    # use .split() function from each string, insert results into name_list arrays
     name_list = name.split()
 
+    # for each part of the list(index) capitalized the first letter.
     for part in name_list:
         print(part[0].upper() + ". ", end="")
+
+    # break to new line
     print()
 
+# execute main function
 main()
--- Python/ch09/ch09#2TheCapitalQuiz.py
+++ Python/ch09/ch09#2TheCapitalQuiz.py
@@ -1,25 +1,34 @@
 # number of trys given
 NUM_STATES = 5
+
 # define main
 def main():
+
     # set dictionary to reference variable
     state_caps = state_cap_dictionary()
+
     # initialize variables
     correct = 0
     incorrect = 0
+
     # for each in number of trys
     for count in range(NUM_STATES):
+
         # select a random state and capital, using .popitem() to assign to 2 variables
         state, capital = state_caps.popitem()
+
         # print question with state item and prompt user
         print('What is the capital of ', state, '? ', end='')
         response = input()
+
         # if response is equal to the answer
         if response.lower() == capital.lower():
+
             # then correct +1
             correct += 1
             print('Correct')
         else:
+
             # else incorrect -1
             incorrect += 1
             print('Incorrect')
@@ -27,7 +36,7 @@ def main():
     # Display the results.
     print('Correct responses:', correct)
     print('Incorrect responses:', incorrect)
-    
+
 # definition of dictionary state capitals
 def state_cap_dictionary():
     sc = {
@@ -83,4 +92,5 @@ def state_cap_dictionary():
     'Wyoming': 'Cheyenne'  }
     return sc
 
+# execute program
 main()
