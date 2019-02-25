a25b84b95bd6dafead27a12d9f75fc52cfb12339
Chris <chris@Chriss-iMac.fios-router.home>
2019-02-25 16:53:18 -0500

    modded examplePlutus

--- Haskell/plutus/examplePlutus.hs
+++ Haskell/plutus/examplePlutus.hs
@@ -20,8 +20,9 @@ data ClearString = ClearString ByteString
 -- Plutus.makelift (not sure where defined) lifts the clear string type to plutus compiler
 PlutusTx.makelift ''ClearString
 
--- Redeemer Script (required to guess locked word in blockchain and redeem reward)
-mkRedeemerScript :: String -> RedeemerScript --newtype RedeemerScript = RedeemerScript { getRedeemer :: Script }
+-- | Redeemer Script (given function to guess locked word in blockchain and redeem reward)
+-- | Redeemer (supplied by consumer of the transaction output)
+mkRedeemerScript :: String -> RedeemerScript -- newtype RedeemerScript = RedeemerScript { getRedeemer :: Script }
 mkRedeemerScript word = -- take a Sring word
     let clearWord = C.pack word --turn word into bytestring
     in RedeemerScript (Ledger.lifted (ClearString clearWord)) -- using data type Clear-ByteString of clearword to compare to datascript for validation.
@@ -35,22 +36,26 @@ gameValidator = ValidatorScript (Ledger.fromCompiledCode $$(PlutusTx.compile [||
         else ($$(P.error) ($$(P.traceH) "Wrong!" ())) -- else false
     ||]))
 
-gameAddress :: Address'
-gameAddress = Ledger.scriptAddress gameValidator
+gameAddress :: Address' -- Address' type defined somewhere
+gameAddress = Ledger.scriptAddress gameValidator -- sets address for gameScript used later for the start of game, and locking of funds to gameValidator address.
 
 lock :: String -> Value -> MockWallet()
 lock word value =
-        payToScript_ gameAddress value (mkDataScript word)
+    payToScript_ gameAddress value (mkDataScript word) -- takes 2 inputs to lock the game, 1st being a word, and then a value of ada.
+    -- ^ function used to pay the gameAddress using the value of ada, and for the datascript associated with all ada transactions; includes our lifted hashed string as posted in (1a) above
 
 guess :: String -> MockWallet ()
 guess word = 
     collectFromScript gameValidator (mkRedeemerScript word)
+-- ^ attempt to collectFromScript using the word in redeemerscript as input for gameValidator to compare to dataScript' HashedWord on-chain.
 
 startGame :: MockWallet ()
 startGame =
     startWatching gameAddress
+-- ^startWatching the actions taken at gameAddress for both wallets (must activate for both wallets)..
 
 $(mkFunction 'lock)
 $(mkFunction 'guess)
 $(mkFunction 'startGame)
 
+-- buttons for functions.
