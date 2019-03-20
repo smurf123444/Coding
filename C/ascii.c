7ea7069f3913d12c9df0db1b401cb573ae95abc7
Chris <chris@Chriss-iMac.fios-router.home>
2019-03-20 18:49:05 -0400

    ascii.c

--- C/ascii.c
+++ C/ascii.c
@@ -1,10 +1,24 @@
+#include <stdio.h>
 
-#include "gunNameChange.h"
+
+void cm_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int cm_putstr(char *str) //cm_putstr(parameter)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		cm_putchar(str[i++]);
+	}
+	return (i);
+}
 
 int main()
 {
-	struct gun gun1;
-	gunInfo('1', "Virginias gun", '9', '6');
-	writeFile();
+	cm_putstr("hey");
+	
 	return (0);
 }
