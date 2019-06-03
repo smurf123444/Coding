13fd900dfedec7b93fc362c32c31b7987afc91a5
unknown <smurf123444@gmail.com>
2019-06-02 23:32:08 -0400

    finished second project SCF VB

--- VB/WindowsApp1/WindowsApp1/Form1.vb
+++ VB/WindowsApp1/WindowsApp1/Form1.vb
@@ -1,20 +1,7 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
-
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
-
-    End Sub
-
+    Const RowA As Decimal = 15D
+    Const RowB As Decimal = 12D
+    Const RowC As Decimal = 9D
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Close()
     End Sub
@@ -23,45 +10,37 @@
         TextBox1.Clear()
         TextBox2.Clear()
         TextBox3.Clear()
-        TextBox4.Clear()
-        TextBox5.Clear()
-        TextBox6.Clear()
-        TextBox7.Clear()
+        TextBox4.Text = String.Empty
+        TextBox5.Text = String.Empty
+        TextBox6.Text = String.Empty
+        TextBox7.Text = String.Empty
+        TextBox1.Focus()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim int1 As Integer = Nothing
-        Dim int2 As Integer = Nothing
-        Dim int3 As Integer = Nothing
-        Dim solution As Integer = Nothing
-
-        Dim int1AsInt As Integer
-        Dim int2AsInt As Integer
-        Dim int3AsInt As Integer
-        If Integer.TryParse(int1, int1asint) Then
-            int1AsInt = TextBox1.Text * 15
-            TextBox6.Text = int1AsInt
-        Else
-            TextBox6.Text = "Not an Integer"
-        End If
-        If Integer.TryParse(int2, int2AsInt) Then
-            int2AsInt = TextBox1.Text * 12
-            TextBox5.Text = int2AsInt
-        Else
-            TextBox5.Text = "Not an Integer"
-        End If
-        If Integer.TryParse(int3, int3AsInt) Then
-            int3AsInt = TextBox1.Text * 9
-            TextBox4.Text = int3AsInt
-        Else
-            TextBox4.Text = "Not an Integer"
-        End If
+        Dim First As Decimal
+        Dim Second As Decimal
+        Dim Third As Decimal
+        Dim Total As Decimal
+        Try
 
 
+            'Calculate A seats  and display revenue for class A seats 
+            First = CDec(TextBox1.Text) * RowA
+            TextBox6.Text = First.ToString("c")
+            'Calculate B seats and display revenue for class A seats 
+            Second = CDec(TextBox2.Text) * RowB
+            TextBox5.Text = Second.ToString("c")
+            'Calculate C seats and display revenue for class A seats 
+            Third = CDec(TextBox3.Text) * RowC
+            TextBox4.Text = Third.ToString("c")
+            'Calculate Total revenue for all seats 
+            Total = First + Second + Third
+            TextBox7.Text = Total.ToString("c")
 
+        Catch
+            MessageBox.Show("Only Numbers Aloud!")
 
-
-        solution = int1AsInt + int2AsInt + int3AsInt
-        TextBox7.Text = solution
+        End Try
     End Sub
 End Class
