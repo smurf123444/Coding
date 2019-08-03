e6f330fb172886d028388c1eaa866c209ab0a1a7
Christopher Manning <smurf123444@gmail.com>
2019-08-03 05:36:35 -0400

    finished motorClass

--- VB/Motor Class/Motor Class/DisplayMotorInput.vb
+++ VB/Motor Class/Motor Class/DisplayMotorInput.vb
@@ -2,66 +2,42 @@
 Public Class DisplayMotorInput
     ' The Get Data procedure assigns values from the form to a motor object's properties
     Private Sub getData(ByVal objMotor As Motor)
-        If Not String.IsNullOrEmpty(txtId.Text) Then
-            Try
-                ' Get all Information
-                objMotor.Motorid = txtId.Text
-                'error check for fun
-            Catch ex As Exception
-                MessageBox.Show("ERROR3: you broke it")
-            End Try
+        Try
+            ' Get all Information
+            objMotor.Motorid = txtId.Text
             'error check for fun
-        Else
-            MessageBox.Show("ERROR2: Enter correct ID value.")
-        End If
-        If Not String.IsNullOrEmpty(txtDesc.Text) Then
-            Try
-                ' Get all Information
-                objMotor.Description = txtDesc.Text
-                'error check for fun
-            Catch ex As Exception
-                MessageBox.Show("ERROR3: you broke it")
-
-            End Try
+        Catch ex As Exception
+            MessageBox.Show("ERROR3: you broke it")
+        End Try
+        'error check for fun
+        Try
+            ' Get all Information
+            objMotor.Description = txtDesc.Text
             'error check for fun
-        Else
-            MessageBox.Show("ERROR2: Enter correct Description value.")
-        End If
-        If Not String.IsNullOrEmpty(txtRPM.Text) Then
-            Try
-                objMotor.MotorRPM = txtRPM.Text
-            Catch ex As Exception
-                'error check for fun
-                MessageBox.Show("ERROR3: you broke it")
-            End Try
+        Catch ex As Exception
+            MessageBox.Show("ERROR3: you broke it")
+            Application.Restart()
+        End Try
+        'error check for fun
+        Try
+            objMotor.MotorRPM = txtRPM.Text
+        Catch ex As Exception
             'error check for fun
-        Else
-            MessageBox.Show("ERROR2: Enter correct RPM value.")
-        End If
-        If Not String.IsNullOrEmpty(txtVoltage.Text) Then
-            Try
-                objMotor.MotorVoltage = txtVoltage.Text
-            Catch ex As Exception
-                'error check for fun
-                MessageBox.Show("ERROR3: you broke it")
-
-            End Try
-        Else
+            MessageBox.Show("ERROR3: you broke it")
+        End Try
+        'error check for fun
+        Try
+            objMotor.MotorVoltage = txtVoltage.Text
+        Catch ex As Exception
             'error check for fun
-            MessageBox.Show("ERROR2: Enter correct Voltage value.")
-        End If
-        If Not String.IsNullOrEmpty(cboStatus.Text) Then
-            Try
-                objMotor.MotorStatus = cboStatus.Text
-
-            Catch ex As Exception
-                'error check for fun
-                MessageBox.Show("ERROR3: you broke it")
-            End Try
-        Else
+            MessageBox.Show("ERROR3: you broke it")
+        End Try
+        Try
+            objMotor.MotorStatus = cboStatus.Text
+        Catch ex As Exception
             'error check for fun
-            MessageBox.Show("ERROR2: Enter correct Status value.")
-        End If
+            MessageBox.Show("ERROR3: you broke it")
+        End Try
     End Sub
     ' ClearForm procedure
     Private Sub clearForm()
@@ -81,19 +57,33 @@ Public Class DisplayMotorInput
         ' Create an instance of the motor class.
         If String.IsNullOrEmpty(txtId.Text) Then
             'error check for real yo
-            MessageBox.Show("ERROR1: Add an ID before proceeding...")
+            MessageBox.Show("Add an ID before proceeding...", "ID field")
         ElseIf String.IsNullOrEmpty(txtDesc.Text) Then
             'error check for real yo
-            MessageBox.Show("ERROR1: Add an Description before proceeding...")
+            MessageBox.Show("Add an Description before proceeding...", "Description field")
         ElseIf String.IsNullOrEmpty(txtRPM.Text) Then
             'error check for real yo
-            MessageBox.Show("ERROR1: Add an RPM before proceeding...")
+            MessageBox.Show("Add an RPM before proceeding...", "RPM field")
         ElseIf String.IsNullOrEmpty(txtVoltage.Text) Then
             'error check for real yo
-            MessageBox.Show("ERROR1: Add an Voltage before proceeding...")
+            MessageBox.Show("Add an Voltage before proceeding...", "Volatage field")
         ElseIf String.IsNullOrEmpty(cboStatus.Text) Then
             'error check for real yo
-            MessageBox.Show("ERROR1: Add an Status before proceeding...")
+            MessageBox.Show("Select an Status before proceeding...", "Status Select")
+        ElseIf isNumeric(txtId.Text) = False Then
+            MessageBox.Show("ID Must be Numeric", "ID field")
+        ElseIf txtID.Text.Length < 5 Or txtID.Text.Length > 5 Then
+            MessageBox.Show("ID Must be a length of 5", "ID field")
+        ElseIf IsNumeric(txtDesc.text) = True Then
+            MessageBox.Show("Description must be text", "Description field")
+        ElseIf isNumeric(txtRPM.Text) = False Then
+            MessageBox.Show("RPM Must be a number", "RPM field")
+        ElseIf txtRPM.Text < 10 Or txtRPM.Text > 10000 Then
+            MessageBox.Show("Rpm must be with in range of 10 - 10000", "RPM field")
+        ElseIf isNumeric(txtVoltage.Text) = False Then
+            MessageBox.Show("Description must be Numeric", "Volatage field")
+        ElseIf txtVoltage.Text < 1 Or txtVoltage.Text > 500 Then
+            MessageBox.Show("Voltage must be with in range of 1 - 500", "Voltage field")
         Else
             Dim objmotor As New Motor
             ' Get data from the form.
@@ -116,8 +106,10 @@ Public Class DisplayMotorInput
         cboStatus.Items.Add("OFF")
         cboStatus.Items.Add("MNT")
         cboStatus.Items.Add("NA")
+        cboStatus.SelectedIndex = 0
     End Sub
 End Class
+
 Public Class Motor
     ' Member variables for ID, Description, RPM, Voltage, Status.
     Public Motorid As String
@@ -144,11 +136,11 @@ Public Class Motor
         End Set
     End Property
     ' RPM Property
-    Public Property MotorRPM() As String
+    Public Property MotorRPM() As Double
         Get
             Return RPM
         End Get
-        Set(ByVal value As String)
+        Set(ByVal value As Double)
             RPM = DisplayMotorInput.txtRPM.Text
         End Set
     End Property
