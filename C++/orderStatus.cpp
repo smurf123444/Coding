6e3944878c0bf6853d73dd6f6cb25c08b4b6200e
Christopher Manning <smurf123444@gmail.com>
2019-09-26 14:24:07 -0400

    modded Order Status

--- C++/orderStatus.cpp
+++ C++/orderStatus.cpp
@@ -2,34 +2,43 @@
 
 using namespace std;
 
-int take()
+int take(int &order, int &stock, int &special)
 {
-	int order, stock, special;
 	int orderref, stockref, specialref;
-	cout << "Order : " << endl;
-	cin >> order;
-	cout << "Stock : " << endl;
-	cin >> stock;
-	cout << "Special : " << endl;
-	cin >> special;
 	orderref = order;
 	stockref = stock;
 	specialref = special;
-	cout << "Order : " << order << " " << "Stock : " << stock << " " << "Special : " << special << endl;
+	cout << "Old Order Val : " << orderref << " Old Stock Val : "<<stockref	 << " Old Special Val : "<< specialref << endl;
+	//cout << "Order : " << order << " " << "Stock : " << stock << " " << "Special : " << special << endl;
+	return (0);
 }
 
 void give(int &ordermod, int &stockmod, int &specialmod)
 {
-	ordermod = 10;
-	stockmod = 10;
-	specialmod = 10;
+	int currentStock = 0, stock, backOrder = 0, readyToShip = 100, totalShipping = 10, totalAmount;
+	stock = 0	;
+	currentStock = stockmod - ordermod;
+	if (currentStock < 0)
+	{
+		backOrder = currentStock * 1;
+	}
+
+	
+	totalShipping = (currentStock * specialmod) + (currentStock * 10);
+
+	cout << "Current Stock: " << currentStock << " Back Order : "<< backOrder << " Ready to ship : " << readyToShip <<" Total Shipping : "<< totalShipping <<endl;
 }
 
 int main()
 {
-
-	cout << "Old Value : " << take() << endl;
-	give()
-	cout << "New Value : " << endl;
+	int order, stock, specialref;
+	cout << "Number of spools Ordered : " << endl;
+	cin >> order;
+	cout << "Number of spools in stock : " << endl;
+	cin >> stock;
+	cout << "Special Shipping charges (with the standard 10$ shipping): " << endl;
+	cin >> specialref;
+	take(order,stock, specialref);
+	give(order, stock, specialref);
 	system("pause");
 }
