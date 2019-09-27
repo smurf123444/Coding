d6ca72e9bc364f9c3b6c391ad89e63ee5c4d56c9
Christopher Manning <smurf123444@gmail.com>
2019-09-27 10:53:45 -0400

    Update orderStatus for simplicity

--- C++/orderStatus.cpp
+++ C++/orderStatus.cpp
@@ -2,34 +2,28 @@
 
 using namespace std;
 
-int take(int &order, int &stock, int &special)
+void take(int &order, int &stock, int &special)
 {
 	int orderref, stockref, specialref;
 	orderref = order;
 	stockref = stock;
 	specialref = special;
-	//cout << "Order : " << order << " " << "Stock : " << stock << " " << "Special : " << special << endl;
-	return (0);
 }
-
-void give(int &order, int &stock, int &special)
+void give(int &order, int &stock, int &shipping)
 {
-	int currentStock = 0, backOrder = 0, readyToShip = 0, totalShipping = 0, totalAmount = 0;
-	currentStock = stock - order;
-	if (currentStock < 0)
+	int currentStock = 0, backOrder = 0, totalPrice = 0, totalShipping = 0, totalAmount = 0;
+	currentStock = stock - order ;
+	totalPrice = currentStock * 100;
+	if (totalPrice < 0)
 	{
-		backOrder = currentStock * 1;
+		totalPrice = stock * 100;
+		backOrder = currentStock * -1;
+		currentStock = currentStock * 0;
+		totalShipping = (10 * stock) + shipping;
 	}
-	else
-	{
-		backOrder = 0;
-	}
-	readyToShip = currentStock * 100;
-	totalShipping = (currentStock * special) + (currentStock * 10);
-	totalAmount = readyToShip + totalShipping;
-	cout << "Stock after Order: " << currentStock << " Back Order : "<< backOrder << " Ready to ship : " << readyToShip <<" Total Shipping : "<< totalShipping << " Total Amount : " << endl;
+	totalAmount = totalShipping + totalPrice;
+	cout << "Stock after Order: " << currentStock << " Back Order : " << backOrder << " Total Price : " << totalPrice << " Total Shipping : " << totalShipping << " Total Amount : " << totalAmount << endl;
 }
-
 int main()
 {
 	int order, stock, specialref;
@@ -39,63 +33,7 @@ int main()
 	cin >> stock;
 	cout << "Special Shipping charges (with the standard 10$ shipping): " << endl;
 	cin >> specialref;
-	take(order,stock, specialref);
+	take(order, stock, specialref);
 	give(order, stock, specialref);
 	system("pause");
 }
-/*	//write a program 1-10 display roman numeral
-	int choice = 0;
-	bool quit = false;
-	do {
-		cout << "Enter value to be converted (0 to exit)" << endl;
-		cin >> choice;
-		while ((choice <= 0) || (choice >= 9))
-		{
-			if ((choice >= 0) && (choice <= 5))
-			{
-				break;
-			}
-			cout << "Enter something 1-5" << endl;
-		}
-		if (choice >= 0 && choice <= 9)
-		{
-			switch (choice)
-			{
-			case 1:
-				cout << "I" << endl;
-				system("pause");
-
-				break;
-			case 2:
-				cout << "II" << endl;
-				system("pause");
-
-				break;
-			case 3:
-				cout << "III" << endl;
-				system("pause");
-
-				break;
-			case 4:
-				cout << "IV" << endl;
-				system("pause");
-
-				break;
-			case 5:
-				cout << "V" << endl;
-				system("pause");
-				break;
-			case 0:
-				cout << "peace out" << endl;
-				quit = true;
-				system("pause");
-				return (0);
-				break;
-			default:
-				std::cout << "Enter the correct input\n" << endl; // no error
-				break;
-			}
-		}
-	} while (!quit);
-}*/
-	
