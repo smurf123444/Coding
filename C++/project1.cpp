ea7cc796b9389df70bbb9cc54150908f40f69c66
Christopher Manning <smurf123444@gmail.com>
2019-10-08 10:05:42 -0400

    Getting close!

--- C++/project1.cpp
+++ C++/project1.cpp
@@ -5,18 +5,17 @@
 #include <algorithm>
 #include <cctype>
 //#include <io.h>
-#include <unistd.h>
+//#include <unistd.h>
 using namespace std;
-
-struct non_alpha 
+bool is_alpha(const std::string& s)
 {
-    bool operator()(char c) {
-        return !std::isalpha(c);
-    }
-};
+	std::string::const_iterator it = s.begin();
+	while (it != s.end() && std::isalpha(*it)) ++it;
+	return !s.empty() && it == s.end();
+}
 int main()
 {
-	non_alpha functor;
+
 	int employeeNumber, location, hoursWorked, i = 0;
 	string firstName, lastName, overTime, locationString, jobTitle;
 	double payRate;
@@ -36,31 +35,21 @@ int main()
 		}
 		cout << "First Name?" << endl;
 		cin >> firstName;
- bool contains = firstName.find("123456789") != string::npos;
-		while(contains == false)
+		while (is_alpha(firstName) != true)
 		{
-			
-			if(firstName[i] > '1' || firstName[i] < '9')
-			{
-			cout << "First Name contains a non alpha character, Enter correct Name" <<endl;
+			cout << "First Name contains a non alpha character, Enter correct Name" << endl;
 			cin >> firstName;
-			contains = firstName.find("123456789") != string::npos;
-			i = 0;
-			}
-			i++;
-		}	
+		}
+
 		cout << "Last Name?" << endl;
 		cin >> lastName;
 
-		while(lastName[i] != '\0')
+		while (is_alpha(lastName) != true)
 		{
-			if(lastName[i] > 1 || lastName[i] < 9)
-			{
-			cout << "First Name contains a non alpha character, Enter correct Name" <<endl;
+			cout << "Last Name contains a non alpha character, Enter correct Name" << endl;
 			cin >> lastName;
-			i++;
-			}
-		}	
+		}
+
 		cout << "Office Location(0 = Tampa; 1 = Sarasota; 2 = Orlando; 3 = Miami)" << endl;
 		cin >> location;
 
@@ -79,68 +68,68 @@ int main()
 			locationString = "Sarasota";
 			cout << "Sarasota" << endl;
 			break;
-		case 2: 
+		case 2:
 			locationString = "Orlando";
 			cout << "Orlando" << endl;
 			break;
-		case 3: 
+		case 3:
 			locationString = "Miami";
 			cout << "Miami" << endl;
 			break;
 		default: cout << "Wrong input" << endl;
 		}
-cout << "Pay Rate ($$.$$)" << endl;
-cin >> payRate;
+		cout << "Pay Rate ($$.$$)" << endl;
+		cin >> payRate;
 		while (payRate < 0 || payRate > 1000)
 		{
 			cout << "Enter a Valid number for pay rate" << endl;
 			cin >> payRate;
 		}
-cout << "Hours Worked" << endl;
-cin >> hoursWorked;
-	while (hoursWorked < 0 || hoursWorked > 99)
-	{
-		cout << "Enter Valid work hours." << endl;
+		cout << "Hours Worked" << endl;
 		cin >> hoursWorked;
-	}
+		while (hoursWorked < 0 || hoursWorked > 99)
+		{
+			cout << "Enter Valid work hours." << endl;
+			cin >> hoursWorked;
+		}
 		cout << "Job Title" << endl;
 		cin >> jobTitle;
-	//place job title code here.
-	if (jobTitle.empty())
-	{
-		cout << "Does not Job Title" << endl;
-	}
-	bool contains_non_alpha4 = find_if(jobTitle.begin(), jobTitle.end(), non_alpha()) != jobTitle.end();
-	while(contains_non_alpha4)
-	{
-  		cout << "Jon Title contains a non alpha character. Enter another job title" << endl;
-		cin >> jobTitle;
-	}
+		//place job title code here.
+		if (jobTitle.empty())
+		{
+			cout << "Does not Job Title" << endl;
+		}
+
+		while (is_alpha(jobTitle) != true)
+		{
+			cout << "Jon Title contains a non alpha character. Enter another job title" << endl;
+			cin >> jobTitle;
+		}
 		cout << "Eligible for Overtime Y = yes N = no" << endl;
 		cin >> eligibleOvertime;
-	switch (eligibleOvertime)
-	{
-	case 'N': overTime = "No";
-		cout << "False" << endl;
-		break;
-	case 'Y': overTime = "Yes";
-		cout << "True" << endl;
-		break;
-	default: overTime = "Error";
-	}
-	if (eligibleOvertime != 'N' && eligibleOvertime != 'Y')
-	{
-	while (eligibleOvertime != 'N' && eligibleOvertime != 'Y')
-	{
-		cout << "Enter Correct Value (N or Y)" << endl;
-		cin >> eligibleOvertime;
-	}
-}
-	//cout << "All values entered correctly" << endl;
-	cout << "All values entered correctly" << endl;
-	cout << employeeNumber << "," << firstName << "," << lastName << "," << locationString << "," << payRate << "," << hoursWorked << "," << jobTitle << "," << overTime << endl;
-	cout << "Quit? 1 for yes 0 for no" << endl;
-	cin >> quit;
+		switch (eligibleOvertime)
+		{
+		case 'N': overTime = "No";
+			cout << "False" << endl;
+			break;
+		case 'Y': overTime = "Yes";
+			cout << "True" << endl;
+			break;
+		default: overTime = "Error";
+		}
+		if (eligibleOvertime != 'N' && eligibleOvertime != 'Y')
+		{
+			while (eligibleOvertime != 'N' && eligibleOvertime != 'Y')
+			{
+				cout << "Enter Correct Value (N or Y)" << endl;
+				cin >> eligibleOvertime;
+			}
+		}
+		//cout << "All values entered correctly" << endl;
+		cout << "All values entered correctly" << endl;
+		cout << employeeNumber << "," << firstName << "," << lastName << "," << locationString << "," << payRate << "," << hoursWorked << "," << jobTitle << "," << overTime << endl;
+		cout << "Quit? 1 for yes 0 for no" << endl;
+		cin >> quit;
 	}
 	fstream fs;
 	fs.open("cop2224_proj1.txt", fstream::in | fstream::out | fstream::app | fstream::ate);
