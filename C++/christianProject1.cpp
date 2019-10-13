5d38c859819551b892ca1205ebf4950a8d24d811
Christopher Manning <smurf123444@gmail.com>
2019-10-13 11:20:12 -0400

    Update christianProject1.cpp

--- C++/christianProject1.cpp
+++ C++/christianProject1.cpp
@@ -1,9 +1,4 @@
-/* Christian Herisse
- Project 1
- The following program will prompt the user to enter
-an employee information
-and will simulate the input file to calculate the payroll records
-*/
+
 
 #include<iostream>
 #include <sstream>
@@ -24,54 +19,39 @@ char eligibleForOT();
 
 
 int main() {
-
-	char i;// employee information
-	cout << " This program is to get an employee information and calculate his or her payroll, press y to continue \n"
-		<< "otherwise press n to quit " << endl;
-	cin >> i;
-
-	while (i) {
-		if (i == 'y') {
-			cout << "The program works Fine" << endl;
-
-			// Open the output file
-			ofstream outputFile;
-			outputFile.open("cop2224_proj1.txt"); // Open outputFile
-			outputFile << employeeNum() << ",";
-			outputFile << Fname() << ",";
-			outputFile << Lname() << ",";
-			outputFile << officeLocation() << ",";
-			outputFile << payRate() << ",";
-			outputFile << hoursWorked() << ",";
-			outputFile << jobTitle() << ",";
-			outputFile << eligibleForOT() << "";
-
-			outputFile.close();
-
-
-		}
-		else if (i == 'n') {
+	bool i;
+	bool quit = false;
+	while (!quit) {
+		
+		cout << "Welcome to the program. 1 for Exit 0 for Continue.." << endl;
+		cin >> i;
+		if (i == true)
+		{
 			return 0;
 		}
-		else
-			cout << "Please enter y or n " << endl;
+				// Open the output file
+				ofstream outputFile;
+				outputFile.open("cop2224_proj1.txt"); // Open outputFile
+				outputFile << employeeNum() << ",";
+				outputFile << Fname() << ",";
+				outputFile << Lname() << ",";
+				outputFile << officeLocation() << ",";
+				outputFile << payRate() << ",";
+				outputFile << hoursWorked() << ",";
+				outputFile << jobTitle() << ",";
+				outputFile << eligibleForOT() << "";
 
-		system("pause");
-		return 0;
-	}
-
+				outputFile.close();
+				cout << "Ready to quit? 1 for yes 0 for no." << endl;
+				cin >> quit;
+			}
+	return (0);
 }
 bool is_alpha(const string& s)
 {
 	string::const_iterator it = s.begin();
 	while (it != s.end() && isalpha(*it)) ++it;
 	return !s.empty() && it == s.end();
-}
-bool is_number(const std::string& s)
-{
-    std::string::const_iterator it = s.begin();
-    while (it != s.end() && std::isdigit(*it)) ++it;
-    return !s.empty() && it == s.end();
 }
 
 int employeeNum() {
@@ -81,12 +61,12 @@ int employeeNum() {
 	if (num <= 999999 || num >= 100000) {
 		return num;
 	}
-	while(num < 100000 || num > 999999) {
+	while (num < 100000 || num > 999999) {
 		cout << "Please Re-enter the number with 6 valid digits" << endl;
 		cin >> num;
 		return num;
 	}
-
+	return (0);
 }
 
 string Fname()
@@ -116,46 +96,51 @@ string Lname() {
 string officeLocation() {
 	string locationString; // employe office location
 	int location;
-		cout << "Office Location(0 = Tampa; 1 = Sarasota; 2 = Orlando; 3 = Miami)" << endl;
+	cout << "Office Location(0 = Tampa; 1 = Sarasota; 2 = Orlando; 3 = Miami)" << endl;
+	cin >> location;
+	//if input is invalid
+	while (location > 3 || location < 0)
+	{
+		cout << "enter correct value for location" << endl;
 		cin >> location;
-		//if input is invalid
-		while (location > 3 || location < 0)
-		{
-			cout << "enter correct value for location" << endl;
-			cin >> location;
-		}
-		//switch case for locations
-		switch (location)
-		{
-		case 0:
-			locationString = "Tampa";
-			cout << "Tampa" << endl;
-			break;
-		case 1:
-			locationString = "Sarasota";
-			cout << "Sarasota" << endl;
-			break;
-		case 2:
-			locationString = "Orlando";
-			cout << "Orlando" << endl;
-			break;
-		case 3:
-			locationString = "Miami";
-			cout << "Miami" << endl;
-			break;
-		default: cout << "Wrong input" << endl;
-		}
+	}
+	//switch case for locations
+	switch (location)
+	{
+	case 0:
+		locationString = "Tampa";
+		cout << "Tampa" << endl;
+		break;
+	case 1:
+		locationString = "Sarasota";
+		cout << "Sarasota" << endl;
+		break;
+	case 2:
+		locationString = "Orlando";
+		cout << "Orlando" << endl;
+		break;
+	case 3:
+		locationString = "Miami";
+		cout << "Miami" << endl;
+		break;
+	default: cout << "Wrong input" << endl;
+	}
 	return (locationString);
 }
 double payRate() {
 	double pR; // employee pay rate 
+	string payRateString;
 	cout << fixed << setprecision(2) << endl;
 	cout << "please enter the employee pay rate: " << endl;
-	cin>> pR;
-	while (pR != true)
+	cin.ignore();
+	getline(cin, payRateString);
+	stringstream(payRateString) >> pR;
+	while ((pR < 0 || pR > 99.99) || (is_alpha(payRateString) == true) ||
+		((payRateString.length() > 5 || payRateString.length() < 5) && payRateString[3] != '.'))
 	{
 		cout << "please enter the number of hours worked by the employee:" << endl;
-		cin >> pR;
+		getline(cin, payRateString);
+		stringstream(payRateString) >> pR;
 	}
 	cout << "" << endl;
 	return pR;
@@ -173,12 +158,12 @@ double hoursWorked() {
 	while (hw < 0 || hw > 99)
 	{
 		cout << "please re-enter the number of hours worked by the employee:" << endl;
-		
+
 		getline(cin, hoursWorked);
 	}
 	cout << "" << endl;
 	return hw;
-		
+
 
 }
 
@@ -187,13 +172,13 @@ string jobTitle() {
 	cout << "please enter the employee job title:" << endl;
 	cin.ignore();
 	getline(cin, jT);
-	while(is_alpha(jT) != true)
+	while (is_alpha(jT) != true)
 	{
 		cout << "please enter the employee job title:" << endl;
 		cin.ignore();
 		getline(cin, jT);
 	}
-	cout << ""<< endl;
+	cout << "" << endl;
 	return jT;
 
 }
@@ -203,7 +188,7 @@ char eligibleForOT() {
 	char oT;
 	cout << "Please enter 'Y' or 'N' if the employee is eligible for overtime:" << endl;
 	cin >> oT;
-	while(oT != 'Y' || oT != 'N')
+	while (oT != 'Y' || oT != 'N')
 	{
 		cout << "Please enter 'Y' or 'N' if the employee is eligible for overtime: " << endl;
 		cin >> oT;
