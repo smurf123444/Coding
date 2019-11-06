475c79abea8134875055ac24055d6502fe95b615
Christopher Manning <smurf123444@gmail.com>
2019-11-06 15:11:09 -0500

    Update CMassignment4.cpp

--- C++/SCF/CMassignment4.cpp
+++ C++/SCF/CMassignment4.cpp
@@ -6,6 +6,7 @@ COP2224 Tues 6-8:50 p.m.
 */
 
 #include <iostream>
+#include <string>
 using namespace std; 
 class Inventory 
 { 
@@ -13,46 +14,74 @@ class Inventory
     private: 
     // Data Members 
     int itemNumber;
+    string description;
     int quantity;
     double cost;
     public: 
     // Member Functions() 
-    int getItemNumber(int in){
+    string setDescription(string des)
+	{
+		description = des;
+        return(description);
+	}
+    int setItemNumber(int in){
         itemNumber = in;
         return itemNumber;
     }
-    int getQuantity(int qn){
+    int setQuantity(int qn){
         quantity = qn;
         return quantity;
     }
-    double getCost(double c){
+    double setCost(double c){
         cost = c;
         return cost;
     }
     //default constructor
-     Inventory() 
-     { 
-       itemNumber = 0;
-       quantity = 0;
-       cost = 0;
-     }
-    void getTotalCost(int item, int quantity, double cost)
+  	Inventory()
+	{
+		description = "";
+		itemNumber = 0;
+		quantity = 0;
+		cost = 0;
+	}
+    Inventory(string desc, int num, int q, double c)
+	{
+		setDescription(desc);
+		setItemNumber(num);
+		setQuantity(q);
+		setCost(c);
+	}
+    void getItemNumber()
+    {
+    cout << " Item Number         : " << itemNumber << endl;
+    }
+    void getQuantity()
+    {
+         cout << " Quantity            : " << quantity << endl;
+    }
+    void getCost()
+    {
+         cout << " Cost                : " << "$" << cost << endl;
+    }
+    void getDescription()
+    {
+        cout << " Description         : " << "\"" << description << "\"" << endl;
+    }
+    void getTotalCost()
      {
          double costTotal;
          costTotal = quantity * cost;
-         cout << " Item ID#: " << getItemNumber(item) << endl;
-         cout << " Quantity: " << getQuantity(quantity) << endl;
-         cout << " Cost Per: " << getCost(cost) << endl;
-         cout << " Cost Total: " << costTotal << endl;
+         cout << " Total Cost          : " << "$" << costTotal << endl;
      }
 };  
 int main() {
     Inventory obj1;
+    string desc;
     int i, q;
     double c;
     bool quit = false;
     cout << " Welcome to Assignment 4 " << endl;
-    cout << " This program is designed to calculate total cost (quantity * item number) " <<endl;
+    cout << " This program is designed to calculate total cost (quantity * cost) " <<endl;
     cout << " Press 0 to Continue and Press 1 to Quit " << endl;
     cin >> quit;
     if(quit){
@@ -61,13 +90,24 @@ int main() {
     while (!quit)
     {
     cout << "Step #" << endl;
-    cout << "1. Enter the Inventory" << endl;
+    cout << "1. Enter the Description" << endl;
+    cin >> desc;
+    cout << "2. Enter the Item Number" << endl;
     cin >> i;
-    cout << "2. Enter the Quantity" << endl;
+    cout << "3. Enter the Quantity" << endl;
     cin >> q;
-    cout << "3. Enter the Cost per Item." << endl;
+    cout << "4. Enter the Cost per Item." << endl;
     cin >> c;
-    obj1.getTotalCost(obj1.getItemNumber(i),obj1.getQuantity(q),obj1.getCost(c));
+    Inventory obj1(desc, i, q, c);
+    cout << "--------------------" << endl;
+    cout << ">>>>>>RESULTS<<<<<<" << endl;
+    cout << "--------------------" << endl;
+    //obj1.setDescription(desc),obj1.setItemNumber(i),obj1.setQuantity(q),obj1.setCost(c)
+    obj1.getDescription();
+	obj1.getItemNumber();
+	obj1.getQuantity();
+	obj1.getCost();
+	obj1.getTotalCost();
     cout << "Press 0 to Continue and Press 1 to Quit" << endl;
     cin >> quit;
     }
