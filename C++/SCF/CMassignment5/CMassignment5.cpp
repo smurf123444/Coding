bfec507552ab512e0145ba33bec6f46bbb1fbcd4
Christopher Manning <smurf123444@gmail.com>
2019-11-19 10:21:15 -0500

    Update CMassignment5.cpp

--- C++/SCF/CMassignment5/CMassignment5.cpp
+++ C++/SCF/CMassignment5/CMassignment5.cpp
@@ -3,9 +3,21 @@ float SavingsAccount::annualInterestRate = .03;
 int main()
 {
 	int l = 0, t = 0;
+	float saved = 0;
 	const int array = 5;
 	SavingsAccount savings[array]= {(2000), (4000), (8000), (16000), (32000)};
 	std::cout << std::endl;
+	while (t < array)
+	{
+		int month; 
+		std::cout << "Original Savings" << std::endl;
+		std::cout << "Item # " << t << std::endl;
+		std::cout << "-------" << std::endl;
+		std::cout << savings[t++].amountKept << std::endl;
+		std::cout << std::endl;
+	} 
+	t = 0;
+	l = 0;
 	while (t < array)
 		{
 			int month;
@@ -13,9 +25,12 @@ int main()
 			std::cout << "item # " << t << std::endl;
 			std::cout << "-----" << std::endl;
 			month = savings[t].calculateMonthlyInterest();
-			l = savings[t++].read(month) + l;
-			std::cout << "Savings balance : $" << l << std::endl;
+			l = savings[t].read(month) + l;
+			std::cout << "Savings balance : $" << l + savings[t++].amountKept << std::endl;
 		}
+	saved = l;
+	std::cout << "Saved Account : " << std::endl;
+	std::cout << saved << std::endl;
 	SavingsAccount::modifyInterestRate(.04);
 	t = 0;
 	l = 0;
@@ -26,7 +41,10 @@ int main()
 		std::cout << "item # " << t << std::endl;
 		std::cout << "-----" << std::endl;
 		month = savings[t].calculateMonthlyInterest();
-		l = savings[t++].read(month) + l;
-		std::cout << "Savings balance : $" << l << std::endl;
+		l = savings[t].read(month) + l;
+		std::cout << "Savings balance : $" << l + savings[t++].amountKept << std::endl;
 	}
+	saved = l + saved;
+	std::cout << "Saved Account : " << std::endl;
+	std::cout << saved << std::endl;
 }
