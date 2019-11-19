50f74aebbde1543b212aa4e62f833ae1f1e89fce
Christopher Manning <smurf123444@gmail.com>
2019-11-19 15:05:12 -0500

    Update CMassignment5.cpp

--- C++/SCF/CMassignment5/CMassignment5.cpp
+++ C++/SCF/CMassignment5/CMassignment5.cpp
@@ -2,25 +2,18 @@
 float SavingsAccount::annualInterestRate = .03;
 int main()
 {
-	int l = 0, t = 0, q = 0;
+	int  t = 0;
+	float l = 0, q = 0;
 	float saved = 0;
 	const int array = 5;
-	//int r[5];
+	float r[5];
+	float h[5];
 	SavingsAccount savings[array]= {(2000), (4000), (8000), (16000), (32000)};
+	
 	std::cout << std::endl;
-	while (t < array)
-	{
-		int month; 
-		std::cout << " Original Savings" << std::endl;
-		std::cout << std::endl;
-		std::cout << " Item # " << t << std::endl;
-		std::cout << "-------" << std::endl;
-		std::cout << std::endl;
-		std::cout << savings[t++].amountKept << std::endl;
-		std::cout << std::endl;
-	} 
-	t = 0;
-	l = 0;
+	std::cout << "Welcome to Assignment 6! This will calculate interest based on declared class array arguments that are sent to the contrustor for its processing" << std::endl;
+	std::cout << "The Variables are $2000, $4000, $8000, $16000, $32000" << std::endl;
+		std::cout << "We will begin by calculating each variable with the interest rate of 3% or .03" << std::endl;
 	while (t < array)
 		{
 			int month;
@@ -32,10 +25,11 @@ int main()
 			std::cout << "  > Item #  " << t << " < " << std::endl;
 			std::cout << std::endl;
 			month = savings[t].calculateMonthlyInterest();
-			l = savings[t].read(month) + l;
-			//r[t] = l;
+			r[t] = savings[t].read(month);
 			std::cout << std::endl;
-			std::cout << " Savings balance for Month 1 Item #" << t << ": $" << l + savings[t++].amountKept << std::endl;
+			std::cout << std::fixed << std::showpoint;
+			std::cout << std::setprecision(2);
+			std::cout << std::fixed << " Savings balance for Month 1 Item #" << t << ": $" << r[t] + savings[t++].amountKept << std::endl;
 			std::cout << std::endl;
 		}
 	saved = l;
@@ -44,12 +38,12 @@ int main()
 	//std::cout << saved << std::endl;
 	SavingsAccount::modifyInterestRate(.04);
 	t = 0;
-	int month;
-	month = saved;
+	float month = 0;
+	
 	//std::cout << "TEST : L VALUE = " << l << std::endl;
 	while (t < array)
 	{
-		
+		month = 0;
 		std::cout << std::endl;
 		std::cout << " /////////////////////////////// " << std::endl;
 		std::cout << std::endl;
@@ -57,16 +51,17 @@ int main()
 		std::cout << std::endl;
 		std::cout << "  > Item #  " << t << " < " << std::endl;
 		std::cout << std::endl;
-		month = savings[t].calculateMonthlyInterest() + month;
-		l = savings[t].read(month);
+		month = savings[t].calculateMonthlyInterest() + r[t];
+		h[t] = savings[t].read(month);
 		std::cout << std::endl;
-		std::cout << " Savings balance for Month 2 Item #" << t << ": $" << l << std::endl;
+		std::cout << " Savings balance for Month 1 + Month 2 Item #" << t << ": $" << h[t] << std::endl;
 		std::cout << " Amount after monthly interest charge (including previous month if applicable) : $";
-		std::cout << savings[t++].amountKept + l << std::endl;
+		std::cout << savings[t].amountKept + h[t++] << std::endl;
 		std::cout << std::endl;
 	}
 	std::cout << " /////////////////////////////// " << std::endl;
 	saved = l + saved;
+	system("pause");
 	//std::cout << "Saved Account : " << std::endl;
 	//std::cout << saved << std::endl;
 }
