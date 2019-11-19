38c5922056162a20845dd98d2236792bdedc38e2
Christopher Manning <smurf123444@gmail.com>
2019-11-19 10:55:10 -0500

    Update Header.hpp

--- C++/SCF/CMassignment5/Header.hpp
+++ C++/SCF/CMassignment5/Header.hpp
@@ -4,21 +4,15 @@ class SavingsAccount {
 	int savingBalance = 0;
 public:
 	static float annualInterestRate;
-	int amountKept;
+	float amountKept;
 	int calculateMonthlyInterest();
 	static void modifyInterestRate(float input);
 	SavingsAccount(){
 		savingBalance = 0;
 		amountKept = 0;
 	}
-	void outPut()
-	{
-		std::cout << "New Saving Balance" << std::endl;
-		std::cout << savingBalance << std::endl;
-	}
 	int read(int num){
 		savingBalance = num + savingBalance;
-		outPut();
 		return (num);
 	}
 	SavingsAccount(float amount,int interestRate = 0) {
