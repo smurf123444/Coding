5e550b16bf4e72b618898d0be9e8e306e37a8134
smurf123444 <smurf123444@gmail.com>
2019-11-20 13:26:21 -0500

    Welcome Project2

---
+++ C++/SCF/Project2/Project2/Header.cpp
@@ -0,0 +1,8 @@
+#include "Header.hpp"
+
+void PayRoll::test(int h)
+{
+	int answer = 0;
+	answer = h * 12;
+	std::cout << answer << std::endl;
+}
---
+++ C++/SCF/Project2/Project2/Header.hpp
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+class PayRoll{
+private:
+	int g, e, t, b, e, r;
+public:
+	PayRoll() {
+		g = 0;
+		e = 0;
+		t = 0;
+		b = 0;
+		e = 0;
+		r = 0;
+	}
+	static void test(int i);
+};
+#endif
