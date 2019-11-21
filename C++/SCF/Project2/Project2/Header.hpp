cf3a56f967370239c8a07079882ae1e058bdadb6
Christopher Manning <smurf123444@gmail.com>
2019-11-21 11:21:47 -0500

    Update Header.hpp

--- C++/SCF/Project2/Project2/Header.hpp
+++ C++/SCF/Project2/Project2/Header.hpp
@@ -1,18 +1,97 @@
 #ifndef HEADER_H
 #define HEADER_H
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+/*
+bool is_alphaQ(const std::string& s)
+{
+	std::string::const_iterator it = s.begin();
+	while (it != s.end() && isalpha(*it)) ++it;
+	return !s.empty() && it == s.end();
+}
+*/
+int employeeNumber()
+{
+	int employeeNumberR = 0;
+	std::string employeeNumberString;
+	bool ready = false;
+	std::cout << "Employee number?" << std::endl;
+	std::cin.ignore();
+	getline(std::cin, employeeNumberString);
+	// to check if it is a valid entry according to a 6 digit number.
+	while (!ready)
+	{
+		if (isalpha(employeeNumberString[6]) == true || isalpha(employeeNumberString[1]) == true /*|| is_alphaQ(employeeNumberString) == true*/)
+		{
+			ready = false;
+			std::cout << "Enter number with out letters." << std::endl;
+			getline(std::cin, employeeNumberString);
+		}
+		std::stringstream(employeeNumberString) >> employeeNumberR;
+		if ((employeeNumberR < 100000 || employeeNumberR > 999999 || employeeNumberString.length() > 6) /*&& is_alphaQ(employeeNumberString) != true*/)
+		{
+			ready = false;
+			std::cout << "Enter number between 10000 and 999999 not " << employeeNumberString << std::endl;
+			getline(std::cin, employeeNumberString);
+		}
+
+		if ((employeeNumberR > 100000 && employeeNumberR < 999999) && isalpha(employeeNumberString[6]) != true /*&& is_alphaQ(employeeNumberString) != true*/)
+		{
+			ready = true;
+		}
+        	
+	}
+    return (employeeNumberR);
+}
+
 class PayRoll{
 private:
-	int g, e, t, b, e, r;
+	int hoursWorked;
+	std::string employeeNumberStringQ, firstName, lastName, payRateString, hoursWorkedString, payAmount;
+	double payRate;
 public:
 	PayRoll() {
-		g = 0;
-		e = 0;
-		t = 0;
-		b = 0;
-		e = 0;
-		r = 0;
+		employeeNumberStringQ = "";
+		hoursWorked = 0;
+		firstName = "";
+		lastName = "";
+		payRateString = "";
+		hoursWorkedString = "";
+		payAmount = "";
+		payRate = 0;
 	}
-	static void test(int i);
+	std::string setEmployeeNumber(std::string h)
+	{
+		employeeNumberStringQ = h;
+		return (h);
+	}
+	std::string setFirstName(std::string h)
+	{
+		firstName = h;
+		return (h);
+	}
+	std::string setLastName(std::string h)
+	{
+		lastName = h;
+		return (h);
+	}
+	std::string setPayRate(std::string h)
+	{
+		payRateString = h;
+		return (h);
+	}
+	std::string setHoursWorked(std::string h)
+	{
+		hoursWorkedString = h;
+		return (h);
+	}
+	std::string setPayAmout(std::string h)
+	{
+		payAmount = h;
+		return (h);
+	}
+	static int increment(int i);
 };
 #endif
