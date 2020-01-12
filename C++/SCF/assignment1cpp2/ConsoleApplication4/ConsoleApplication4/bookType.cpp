aea47ef58e93cfb1f85f2466a1e6a291012c2a25
Christopher Manning <smurf123444@gmail.com>
2020-01-11 20:39:28 -0500

    Update bookType.cpp

--- C++/SCF/assignment1cpp2/ConsoleApplication4/ConsoleApplication4/bookType.cpp
+++ C++/SCF/assignment1cpp2/ConsoleApplication4/ConsoleApplication4/bookType.cpp
@@ -4,6 +4,7 @@ BookType setInfo(BookType obj) {
 	BookType input;
 	string titleInput;
 	string authorInput;
+	string publisherInput;
 	string ISBNinput;
 	double priceInput;
 	int stockInput;
@@ -23,12 +24,15 @@ BookType setInfo(BookType obj) {
 			authorInput = "";
 			i = 4;
 		}
-		
+
 		i++;
 		t++;
 	}
 	obj.authorCount = t;
-	cout << "ISBN # : " << endl;
+	cout << "Set Publisher : " << endl;
+	cin >> publisherInput;
+	obj.setPublisher(publisherInput);
+	cout << "Set ISBN # : " << endl;
 	cin >> ISBNinput;
 	obj.setISBN(ISBNinput);
 	cout << "Set Price : " << endl;
@@ -46,6 +50,9 @@ void searchTitle(string search, BookType obj) {
 void searchISBN(string search, BookType obj) {
 	obj.checkISBN(search);
 }
+void searchPublisher(string search, BookType obj) {
+	obj.checkPublisher(search);
+}
 int main() {
 	BookType obj[10];
 	bool quit = false;
@@ -53,79 +60,84 @@ int main() {
 	int bookChoice;
 	string titleSearch;
 	string ISBNSearch;
-		cout << ">>>>>>>>>>>>>>>>>>>>>>>" << endl;
-		cout << "Welcome to Assignment 1 for CPP 2 SCF!" << endl;
-		while (!quit) {
-			cout << "Select one of the following options" << endl;
-			cout << "1. Enter Book Information" << endl;
-			cout << "2. Update Book Information" << endl;
-			cout << "3. Search Title" << endl;
-			cout << "4. Search ISBN" << endl;
-			cout << "5. Exit" << endl;
-			cout << endl;
-			cout << "Type Here then press [Enter]: ";
-			cin >> selection;
-			int i = 0;
-			string leave;
-			switch (selection) {
-			case 1: 
-				while (i < 10) {
-					cout << "Enter 0 to Continue Adding to the list of Books, or 1 for End of List" << endl;
-					cin >> leave;
-					if (leave == "1")
-					{
-						i = 10;
-					}
-					else {
-						obj[i] = setInfo(obj[i]);
-					}
-					i++;
-				}
-				i = 0;
-				break;
-			case 2: cout << "Which Book would you like to update? 1-10" << endl;
-				cin >> bookChoice;
-				obj[bookChoice - 1] = setInfo(obj[bookChoice - 1]);
-				break;
-			case 3: 
-				cout << "Which Title are you searching for?" << endl;
-				cin >> titleSearch;
-				while (i < 10) {
-					searchTitle(titleSearch, obj[i++]);
-				}
-				i = 0;
-				break;
-			case 4:
-				cout << "Which ISBN are you searching for?" << endl;
-				cin >> ISBNSearch;
-				while (i < 10) {
-					searchISBN(ISBNSearch, obj[i++]);
-				}
-				break;
-			case 5:
-				cout << "Exiting.." << endl;
-				quit = true;
-				break;
-			default: break;
-			};
-			i = 0;
-			int fun = 1;
+	cout << ">>>>>>>>>>>>>>>>>>>>>>>" << endl;
+	cout << "Welcome to Assignment 1 for CPP 2 SCF!" << endl;
+	while (!quit) {
+		cout << "Select one of the following options" << endl;
+		cout << "1. Enter Book Information" << endl;
+		cout << "2. Update Book Information" << endl;
+		cout << "3. Search Title" << endl;
+		cout << "4. Search ISBN" << endl;
+		cout << "5. Exit" << endl;
+		cout << endl;
+		cout << "Type Here then press [Enter]: ";
+		cin >> selection;
+		int i = 0;
+		string leave;
+		switch (selection) {
+		case 1:
 			while (i < 10) {
-				cout << "TEST" << endl;
-				cout << "Book # :" << fun++ << endl;
-				cout << "Title : " << obj[i].getTitle() << endl;
-				cout << "Author : ";
-				obj[i].getAuthor();
-				cout << "Publisher : ";
-				cout << obj[i].getPublisher();
-				cout << "ISBN # " << endl;
-				cout << obj[i].getISBN();
-				cout << "Price : " << endl;
-				cout << obj[i].getPrice();
-				cout << "Stock : " << endl;
-				cout << obj[i++].getStock();
-				cout << endl;
+				cout << "Enter 0 to Continue Adding to the list of Books, or 1 for End of List" << endl;
+				cin >> leave;
+				if (leave == "1")
+				{
+					i = 10;
+				}
+				else {
+					obj[i] = setInfo(obj[i]);
+				}
+				i++;
 			}
+			i = 0;
+			break;
+		case 2: cout << "Which Book would you like to update? 1-10" << endl;
+			cin >> bookChoice;
+			obj[bookChoice - 1] = setInfo(obj[bookChoice - 1]);
+			break;
+		case 3:
+			cout << "Which Title are you searching for?" << endl;
+			cin >> titleSearch;
+			while (i < 10) {
+				searchTitle(titleSearch, obj[i++]);
+			}
+			i = 0;
+			break;
+		case 4:
+			cout << "Which ISBN are you searching for?" << endl;
+			cin >> ISBNSearch;
+			while (i < 10) {
+				searchISBN(ISBNSearch, obj[i++]);
+			}
+			break;
+		case 5:
+			cout << "Exiting.." << endl;
+			quit = true;
+			break;
+		default: break;
+		};
+		i = 0;
+		int fun = 1;
+		while (i < 10) { 
+			cout << "Book # : " << fun++ << endl;
+			cout << "-----------------------" << endl; 
+			cout << "Title : " << obj[i].getTitle() << endl;
+			cout << "Author : ";
+			obj[i].getAuthor();
+			cout << endl;
+			cout << "Publisher : ";
+			cout << obj[i].getPublisher();
+			cout << endl;
+			cout << "ISBN # ";
+			cout << obj[i].getISBN();
+			cout << endl;
+			cout << "Price : ";
+			cout << obj[i].getPrice();
+			cout << endl;
+			cout << "Stock : ";
+			cout << obj[i++].getStock();
+			cout << endl;
+			cout << "-----------------------" << endl;
+		}
 	}
 	return (0);
 }
