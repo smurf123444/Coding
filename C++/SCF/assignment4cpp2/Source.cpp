daf72fbb55afa3651e71291a2d94ae9745faf13e
Chris <smurf123444@gmail.com>
2020-03-21 22:10:57 -0400

    added findKthElement

--- C++/SCF/assignment4cpp2/Source.cpp
+++ C++/SCF/assignment4cpp2/Source.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+#include "linkedList.h"
+#include "orderedLinkedList.h"
 #include <string>
 #include <sstream>
 int main()
 {
+	orderedLinkedList<int> obj;
+	obj.insert(3);
+	obj.insert(4);
+	obj.insert(1);
+	obj.findKthElement(2);
 
 	return (0);
 
--- C++/SCF/assignment4cpp2/linkedList.h
+++ C++/SCF/assignment4cpp2/linkedList.h
@@ -170,6 +170,10 @@ public:
       //               points to the last node of the updated 
       //               list, and count is decremented by 1.
 
+    void findKthElement(int k);
+
+    void deleteKthElement(Type& k);
+
     linkedListIterator<Type> begin();
       //Function to return an iterator at the begining of the 
       //linked list.
@@ -208,6 +212,24 @@ private:
       //               assigned to this list.
 };
 
+template <class Type>
+void linkedListType<Type>::findKthElement(int kth)
+{
+    nodeType<Type> *current; //pointer to traverse the list
+
+    current = first;    //set current so that it points to 
+          int i = 0;              //the first node
+    while (current != nullptr) //while more data to print
+    {
+        if(kth == i)
+        {
+            cout << current->info << " ";
+            current = current->link;
+        } 
+         i++;
+    }
+	 //if the list has any nodes, delete them
+}
 
 template <class Type>
 bool linkedListType<Type>::isEmptyList() const
