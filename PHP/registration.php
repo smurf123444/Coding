adcd0274477b3cbf37648afe926d6855dd419e1e
Coder <smurf123444@gmail.com>
2023-02-26 10:50:22 -0500

    Update registration.php

--- PHP/registration.php
+++ PHP/registration.php
@@ -3,8 +3,8 @@
 <body>
 
 <?php
-$db = "zero_auth";
-$con = mysqli_connect("127.0.0.1","root","", $db);
+$db = "acore_auth";
+$con = mysqli_connect("localhost:3307","root","12341234", $db);
 
 function encrypt($user, $pass) {
   $user = strtoupper($user);
@@ -28,12 +28,42 @@ if (!mysqli_select_db($con, $db))
     $expansion = $_POST['expansion'];
     $password = $_POST['sha_pass_hash'];
     $password2 = $_POST['sha_pass_hash2'];
+    // generate a random salt
+$salt = random_bytes(32);
+
+
+function CalculateSRP6Verifier($username, $password, $salt)
+{
+    // algorithm constants
+    $g = gmp_init(7);
+    $N = gmp_init('894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7', 16);
+
+    // calculate first hash
+    $h1 = sha1(strtoupper($username . ':' . $password), TRUE);
+
+    // calculate second hash
+    $h2 = sha1($salt.$h1, TRUE);
+
+    // convert to integer (little-endian)
+    $h2 = gmp_import($h2, 1, GMP_LSW_FIRST);
+
+    // g^h2 mod N
+    $verifier = gmp_powm($g, $h2, $N);
+
+    // convert back to a byte array (little-endian)
+    $verifier = gmp_export($verifier, 1, GMP_LSW_FIRST);
+
+    // pad to 32 bytes, remember that zeros go on the end in little-endian!
+    $verifier = str_pad($verifier, 32, chr(0), STR_PAD_RIGHT);
     
-    
+    return $verifier;
+}
+$verifierOutput = CalculateSRP6Verifier($_POST['username'],$_POST['sha_pass_hash'],$salt);
+
     if ($password == $password2)
     {
         $password = encrypt($username, $_POST['sha_pass_hash']); //hash the password :D
-        $sql = "INSERT INTO account (username, sha_pass_hash, expansion, email) VALUES ('$username', '$password', '$expansion', '$email')";
+        $sql = "INSERT INTO account (username, salt, verifier, expansion, email) VALUES ('$username', '$salt', '$verifierOutput','$expansion', '$email')";
         mysqli_query($con, $sql);
         echo "Query executed";
         header("refresh=2;url=index.php");
